4d98659592c390c6e08f73e9f17938bd93128005
Leonardo Arruda <la000476@gmail.com>
2025-11-17 23:52:38 +0000

    AtividadeIV

---
+++ CalculoIRRF/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o salário bruto: ");
+        string? entrada = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(entrada))
+        {
+            Console.WriteLine("Valor inválido. Encerrando o programa.");
+            return;
+        }
+
+        decimal salarioBruto = decimal.Parse(entrada);
+
+        CalculadoraINSS calcInss = new CalculadoraINSS();
+        CalculadoraIRRF calcIrrf = new CalculadoraIRRF();
+
+        decimal descontoINSS = calcInss.Calcular(salarioBruto);
+        decimal salarioBase = salarioBruto - descontoINSS;
+        decimal descontoIRRF = calcIrrf.Calcular(salarioBase);
+
+        Console.WriteLine($"\nSalário Bruto: {salarioBruto:C}");
+        Console.WriteLine($"Desconto INSS: {descontoINSS:C}");
+        Console.WriteLine($"Salário Base: {salarioBase:C}");
+        Console.WriteLine($"Desconto IRRF: {descontoIRRF:C}");
+    }
+}
